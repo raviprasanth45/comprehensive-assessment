ccf5919904e15fca3e76003da3d4cf52e341bf04
unknown <mindtreedec353@iiht.tech>
2022-03-29 15:29:53 +0530

    First commit

---
+++ src/main/java/com/comprehensiveJava/springbootcrud/Welcome.java
@@ -0,0 +1,11 @@
+package com.comprehensiveJava.springbootcrud;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to integration");
+
+	}
+
+}
